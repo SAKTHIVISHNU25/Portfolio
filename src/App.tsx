66735e6b11e34d97664423ccd9deae992989957f
sakthivishnu <79744876+SAKTHIVISHNU25@users.noreply.github.com>
2024-12-04 18:55:30 +0530

    environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 // import { useEffect } from 'react'
-import { Footer } from './components/Footer/Footer'
-import { Header } from './components/Header/Header'
-import { Main } from './components/Main/Main'
-import { GlobalStyle } from './styles/global'
-import 'react-toastify/dist/ReactToastify.css'
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+import { GlobalStyle } from "./styles/global";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
-    <>
+    <div style={{ backgroundColor: "red" }}>
       <GlobalStyle></GlobalStyle>
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
